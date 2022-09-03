9d680ffc4c6f776451b35bb39e59eade846e5305
CryWhat <miltonbarrientos2@gmail.com>
2022-09-02 23:02:57 -0600

    Generar matriz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  matriz: any[] = [];
+  constructor(private primengConfig: PrimeNGConfig) {
+    this.primengConfig.ripple = true;
+    this.crearMatriz();
+  }
+
   title = 'WarshallWeb';
+  matrizSize = 5;
+
+  crearMatriz() {
+    this.matriz = [];
+    for (let i = 0; i < this.matrizSize; i++) {
+      this.matriz.push([]);
+      for (let j = 0; j < this.matrizSize; j++) {
+        this.matriz[i].push({ valor: 0 });
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
+// PrimeNG
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
+import { InputSwitchModule } from 'primeng/inputswitch';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    InputNumberModule,
+    FormsModule,
+    ButtonModule,
+    InputSwitchModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
